e240cf50172ea2a8d7509f0e5ed64bea49cd220b
mdhassan91 <mohammedhassan8055006667@gmail.com>
2024-07-09 22:24:32 +0200

    My Message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Sohan from './Sohan';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,12 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>Sohan is Gandu</h1>
+      <button>
+        Hello
+      </button>
+      <Sohan/>
     </div>
   );
 }
